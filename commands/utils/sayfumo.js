b73a496c5545b7ae3ffe25d6307a422c85b537b4
ikin-dev <ikin-dev@users.noreply.github.com>
2026-04-27 04:07:31 +0300

    Init

---
+++ commands/utils/sayfumo.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("sayfumo")
+    .setDescription("Says FUMO!!!!"),
+    async execute(interaction) {
+      await interaction.reply("FUMO!!!!");
+    },
+};
