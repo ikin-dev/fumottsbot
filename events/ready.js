b82d699228fd8b1b42c8f7c81a8e88bb0f27d4e7
ikin-dev <ikin-dev@users.noreply.github.com>
2026-04-29 23:47:25 +0300

    Remove test comments

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,9 @@
 const { Events } = require("discord.js");
-const { saveUserData } = require("../db/userDataHandler.js");
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		
-		// for testing jsonHandler, userDataHandler, and db.json
-		// await saveUserData("6767676767", "testuser", "testvoice");
 	},
 };
